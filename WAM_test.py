881020e549b142836e335744e42626f389ead438
Ian Abraham <ianabraham21@gmail.com>
2015-11-08 17:04:23 -0600

    Added WAM body axis

--- WAM_test.py
+++ WAM_test.py
@@ -9,14 +9,15 @@ L1 = 550*1e-3
 L2 = 300*1e-3
 L3 = 60*1e-3
 W1 = 45*1e-3
-"""
-B6 = [0,0,1,0,0,0]
-B5 = [0,-1,0,-W2,0,0]
-B4 = [0,0,1,H2,0,0]
-B3 = [0,0,1,H2,-L2,0]
-B2 = [0,0,1,H2,-L2-L1,0]
-B1 = [0,1,0,W1+W2,0,L1+L2]
-"""
+
+B7 = [0,0,1,0,0,0]
+B6 = [0,1,0,L3,0,0]
+B5 = [0,0,1,0,0,0]
+B4 = [0,1,0,L2+L3,0,W1]
+B3 = [0,0,1,0,0,0]
+B2 = [0,1,0,L1+L2+L3,0,0]
+B1 = [0,0,1,0,0,0]
+
 S7 = [0,0,1,0,0,0]
 S6 = [0,1,0,-L1-L2,0,0]
 S5 = [0,0,1,0,0,0]
@@ -25,7 +26,7 @@ S3 = [0,0,1,0,0,0]
 S2 = [0,1,0,0,0,0]
 S1 = [0,0,1,0,0,0]
 
-#Bi = [B1,B2,B3,B4,B5,B6]
+Bi = [B1,B2,B3,B4,B5,B6,B7]
 Si = [S1,S2,S3,S4,S5,S6,S7]
 
 M = [[1,0,0,0],
@@ -47,8 +48,8 @@ theta0 = 0.1*np.random.randn(7)#
 #theta0 = [-0.00686114  0.00922843  0.00602058 -0.00167514  0.00557784  0.00661908]
 print "Initial Joint Angles: "
 print theta0
-#Theta = IKinBody(Bi, M, Tsd, theta0)
-Theta = IKinFixed(Si, M, Tsd, theta0)
+Theta = IKinBody(Bi, M, Tsd, theta0)
+#Theta = IKinFixed(Si, M, Tsd, theta0)
 if saveData==True:
     size = np.shape(Theta)
     import csv
