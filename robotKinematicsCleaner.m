3b10c48c7ab382a3c6d56863cb7002c306b67061
Ian Abraham <ianabraham21@gmail.com>
2015-11-13 09:22:31 -0600

    New Organization

--- robotKinematicsCleaner.m
+++
@@ -1,7 +0,0 @@
-data = csvread('newdata.csv')
-
-characterPara = ["time","shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint"];
-newdata = [characterPara;data(:,1),mod(abs(data(:,2:end)),pi).*sign(data(:,2:end))]
-
-
-csvwrite('scriptTest_temp.csv',newdata)
--- testUtil.py
+++
@@ -1,7 +0,0 @@
-from robotUtil import *
-
-Theta = [pi/8,pi/2,pi/4]
-Si =  [[0,0,-1,2,0,0],[0,0,0,0,1,0],[0,0,1,0,0,0.1]]
-
-print FixedJacobian(Theta, Si)
-print BodyJacobian(Theta, Si)
